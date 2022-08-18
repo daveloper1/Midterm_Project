898aab817c3e529f9c80914618b8e535ff0f8737
David Leon <97davidleon@gmail.com>
2022-08-18 14:14:22 +0200

    JS modified to change the project image according to the project

--- code.js
+++ code.js
@@ -24,11 +24,54 @@ function redirectContacUs() {
   console.log("hoolaaaa");
 }
 
+function getParams() {
+  console.log("ueeee");
+}
+
 function redirectProject() {
-  console.log("aaaaaaaaaaaaaa");
-  const newLocation = this.querySelector('a').getAttribute("href");
-  window.location = newLocation;
-  
+  /* Get the new location of the "Learn more" and change the title & subtitles*/
+  const newLocation = this.querySelector("a").getAttribute("href");
+  const title = this.querySelector(".project-title").innerHTML;
+  const subtitle = this.querySelector(".project-description").innerHTML;
+  window.location.href = newLocation + `?title=${title}&subtitle=${subtitle}`;
+}
+
+function QueryPage(params) {
+  const title = params.title;
+  const subtitle = params.subtitle;
+  const titleBox = document.getElementsByClassName("title")[0];
+  const subtitleBox = document.getElementsByClassName("subtitle")[0];
+
+  titleBox.innerHTML = title;
+  subtitleBox.innerHTML = subtitle;
+
+  let imageSelector = 1;
+  switch (title.toLowerCase()) {
+    case "simplify":
+      imageSelector = 1;
+      break;
+    case "dashcoin":
+      imageSelector = 2;
+      break;
+    case "vectorify":
+      imageSelector = 3;
+      break;
+    case "orbit":
+      imageSelector = 4;
+      break;
+    case "purify":
+      imageSelector = 5;
+      break;
+    case "cryptopie":
+      imageSelector = 6;
+      break;
+  }
+
+  const imageBox = document.getElementsByClassName("image-title")[0];
+  console.log(imageBox.style);
+  imageBox.style.backgroundImage = `url(project-assets/projects-section/${imageSelector}.jpg)`;
+  const bgImageBox = document.getElementsByClassName("image-title-bg")[0];
+  bgImageBox.style.backgroundImage = `url(project-assets/projects-section/${imageSelector}.jpg)`;
 }
 
 window.addEventListener("load", () => {
@@ -38,18 +81,19 @@ window.addEventListener("load", () => {
   for (let i = 0; i < subscribeBtnarr.length; i++) {
     subscribeBtnarr[i].addEventListener("click", redirectContacUs);
   }
-
+  /*----------------------------------------------------------------------------*/
   const projectCard = document.getElementsByClassName("project");
   let projectCardArr = [...projectCard];
 
   for (let i = 0; i < projectCardArr.length; i++) {
     projectCardArr[i].addEventListener("click", redirectProject);
   }
-
-  /*
-  $(".project-link").click(function () {
-    console.log("AJAAAA");
-    window.location = $(this).find("a").attr("href");
-    return false;
-  });*/
+  var params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+  if (params.title != null) {
+    /* console.log(params.title);
+    console.log(params.subtitle);*/
+    QueryPage(params);
+  }
 });
